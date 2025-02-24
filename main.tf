9afd1c1caf87bd69c3259399b996579d5cf6437f
Oluwasola <ademolaoluwasola@gmail.com>
2025-02-24 16:43:19 -0700

    Modified terraform script

--- main.tf
+++ main.tf
@@ -3,15 +3,7 @@ provider "aws" {
 }
 
 module "security_group" {
-    source = ".modules/security_group"
+    source = "./modules/security_group"
     vpc_id = var.vpc_id
     environment = var.environment
-}
-
-output "alb_sg_id" {
-    value = module.security_group.alb_sg_id
-}
-
-output "ecs_sg_id" {
-    value = module.security_group.ecs_sg_id
 }
