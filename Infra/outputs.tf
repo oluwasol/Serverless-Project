764188da4b80ac23a0b7bbeb62947c61972d55e7
Oluwasola <ademolaoluwasola@gmail.com>
2025-02-24 23:01:00 -0700

    Modified terraform script again and again

--- Infra/outputs.tf
+++ Infra/outputs.tf
@@ -1,7 +1,7 @@
 output "alb_sg_id" {
-  value = aws_security_group.alb_sg_id
+  value = module.security_group.alb_sg_id
 }
 
 output "ecs_sg_id" {
-  value = aws_security_group.ecs_sg_id
+  value = module.security_group.ecs_sg_id
 }
