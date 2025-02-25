7aba75f7ac7bd6feaa20ae09354fe90e783fb731
Oluwasola <ademolaoluwasola@gmail.com>
2025-02-24 22:18:48 -0700

    Modified terraform script again and again

--- Infra/security_group/variables.tf
+++ Infra/security_group/variables.tf
@@ -1,7 +1,7 @@
-variable "vpc_id" {
-  description = "The ID of the VPC where resources will be created"
-  type = string
-}
+#variable "vpc_id" {
+  #description = "The ID of the VPC where resources will be created"
+  #type = string
+#}
 
 variable "environment" {
   description = "Environment name (dev , qut , prod)"
